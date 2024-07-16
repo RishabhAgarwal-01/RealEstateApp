3b64b2c30bc93d8396be4831f94fcbb9ac34032e
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-16 10:50:45 +0530

    Made the pages for the authentication

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Profile from "./pages/Profile.jsx";
+import About from "./pages/About.jsx";
+
+function App() {
+  return (
+  <BrowserRouter>
+
+  <Routes>
+       <Route path='/' element={<Home/>} />
+       <Route path='/sign-in' element={<SignIn/>} />
+       <Route path='/sign-up' element={<SignUp/>} />
+       <Route path='/about' element={<About/>} />
+       <Route path='/profile' element={<Profile/>} />
+  </Routes>
+      
+  </BrowserRouter>
+  )
+  
+}
+
+export default App
