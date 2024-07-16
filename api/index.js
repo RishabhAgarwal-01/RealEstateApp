44eaa8b45a402e222543090080af1c8e8421d5f2
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-16 17:08:52 +0530

    creted a signup api route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
+//Mongo DB connection
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to DB")
 })
@@ -13,6 +15,29 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+//express app instance
+const app = express();
+
+//middleware for allowing the app to send the JSON data to the backend expres server 
+app.use(express.json());
+
+//server listening
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
+//user route
+app.use('/api/user', userRouter);
+//auth routes
+app.use('/api/auth', authRouter);
+
+//middleware for the error handling
+app.use((err,req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
 })
