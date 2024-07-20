d04bff16f18a87cb7886b50d643028794ef46c38
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-20 11:48:43 +0530

    Made the Create listing UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./pages/Profile.jsx";
 import About from "./pages/About.jsx";
 import Header from './components/Header.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
+import CreateListing from './pages/CreateListing.jsx'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
        <Route path='/about' element={<About/>} />
        <Route element={<PrivateRoute/>}>
           <Route path='/profile' element={<Profile/>} />
+          <Route path='/create-listing' element={<CreateListing/>} />
        </Route>
   </Routes>
       
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,6 +16,7 @@ import {
   deleteUserSuccess,
   signOutUserStart,
 } from '../redux/user/userSlice';
+import {Link} from "react-router-dom";
 
 function Profile() {
 
@@ -192,6 +193,13 @@ function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
 
       </form>
 
