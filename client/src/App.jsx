52779d2db2344ecf4e8e94a1fe798f46f95aa610
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-21 14:17:31 +0530

    completed update user functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import About from "./pages/About.jsx";
 import Header from './components/Header.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import CreateListing from './pages/CreateListing.jsx'
+import UpdateListing from './pages/UpdateListing.jsx'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
        <Route element={<PrivateRoute/>}>
           <Route path='/profile' element={<Profile/>} />
           <Route path='/create-listing' element={<CreateListing/>} />
+          <Route path='/update-listing/:listingId' element={<UpdateListing/>} />
        </Route>
   </Routes>
       
