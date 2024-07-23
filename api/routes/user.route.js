7600cb0771168c252ab2ea82bedea24955cb30df
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-23 13:07:02 +0530

    completed the listing page functionality and contact owner fucntionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateUser, deleteUser, getUserListings} from "../controllers/user.controller.js";
+import { updateUser, deleteUser, getUserListings, getUser} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 
@@ -11,5 +11,6 @@ router.post('/update/:id',verifyToken, updateUser);
 router.delete('/delete/:id',verifyToken, deleteUser);
 // show all listing
 router.get('/listings/:id',verifyToken, getUserListings);
-
+//
+router.get('/:id', verifyToken, getUser);
 export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,8 @@ import Header from './components/Header.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import CreateListing from './pages/CreateListing.jsx'
 import UpdateListing from './pages/UpdateListing.jsx'
-
+import Listing from './pages/Listing.jsx'
+import { useEffect } from 'react'
 function App() {
   return (
   <BrowserRouter>
@@ -19,6 +20,7 @@ function App() {
        <Route path='/sign-in' element={<SignIn/>} />
        <Route path='/sign-up' element={<SignUp/>} />
        <Route path='/about' element={<About/>} />
+       <Route path='/listing/:listingId' element={<Listing/>}/>
        <Route element={<PrivateRoute/>}>
           <Route path='/profile' element={<Profile/>} />
           <Route path='/create-listing' element={<CreateListing/>} />
